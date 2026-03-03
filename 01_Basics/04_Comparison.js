0c09ac854a0711832b3d6bd85401c55f7ac49589
Manoja S S <manojass.in@gmail.com>
2026-03-03 12:16:33 +0000

    Learn: Comparison of Datatype in JS

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,28 @@
+// { Basics Of Comparison } In Comparison if 2 Oprends are numbers ==> then they are the best Comparison
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+
+// Here datatype is Changed it is Confusing
+// console.log("18" > 2);
+// console.log("02" > 1);
+
+// This Comparison Can Confuse you at any time
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// Here everthing will be false
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+// === Will Checks the datatype
+
+console.log("02" === 2);
