cd0f4ca2cf29ab511872bf9dfe98510015a56342
Manoja S S <manojass.in@gmail.com>
2026-03-27 18:09:46 +0000

    Learn: about Number and Maths

---
+++ 01_Basics/06_number_and_maths.js
@@ -0,0 +1,19 @@
+const num = 400;
+// console.log(num);
+
+const newNum = new Number(1800.18);
+
+// console.log(newNum.toString());
+// console.log(newNum.toFixed(2));
+// console.log(newNum.toPrecision(5));
+
+// ++++++++++++++++++++ Maths ++++++++++++++++
+
+// console.log(Math.PI);
+// console.log(Math.floor(18.88));
+// console.log(Math.floor((Math.random()*10))+1);
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor((Math.random()*(max - min + 1))+1)); // getting random number form 1 to 6
