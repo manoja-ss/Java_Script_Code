379447c60f92facd494b08d6cd25979ceb2e558d
Manoja S S <manojass.in@gmail.com>
2026-03-27 07:21:49 +0000

    summary of datatype

---
+++ 01_Basics/datatype_summary.js
@@ -0,0 +1,20 @@
+// Primitive DataType 
+
+// Number, string, boolean, null, undefined, bigInt
+
+// Non Primitive DataType
+
+// array, object, function
+
+const arr = ["Manoja", "Abhi", "Bathu"];
+
+
+const myObj = {
+    name : "Manoja S S",
+    age : 22
+}
+const printName = function(){
+    console.log("My Name is Manoja");
+}
+console.log(arr);
+console.log(myObj);
