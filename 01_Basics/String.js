49338758e94c0f0ef93334f8671b8035c2fbbf18
Manoja S S <manojass.in@gmail.com>
2026-03-27 16:59:26 +0000

    Learn: About Strings

---
+++ 01_Basics/String.js
@@ -0,0 +1,11 @@
+const name = "Manoja"; // We should not create and Initialize string like this 
+
+const myName = new String('Manoja S S'); // we should use this for creating a string
+
+// console.log(myName.toUpperCase());
+
+const anotherName = "Abhi";
+const myAge = 22;
+const printText = `My Name is ${anotherName} I am ${myAge} Years Old.` // best for concatenation of Strings
+
+console.log(printText);
